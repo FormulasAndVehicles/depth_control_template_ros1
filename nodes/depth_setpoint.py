8e2b27ca3798f3e9c7a27e3e5a1db74ef0074961
NBauschmann <nathalie_bauschmann@hotmail.de>
2021-10-26 14:14:30 +0200

    add square wave setpoint example

--- nodes/depth_setpoint.py
+++ nodes/depth_setpoint.py
@@ -5,19 +5,45 @@ from std_msgs.msg import Float64
 
 class DepthSetpointNode():
     def __init__(self):
-        rospy.init_node("depth_setpoints")
+        rospy.init_node("depth_setpoint_publisher")
+
+        self.start_time = rospy.get_time()
+
+        # change these parameters to adjust setpoint(s)
+        # either constant setpoint (setpoint_1), or jumping between two setpoints
+        self.constant = False  # constant or square wave?
+        self.duration = 30.0  # in s
+        self.setpoint_1 = -0.4
+        self.setpoint_2 = -0.6
 
         self.setpoint_pub = rospy.Publisher("depth_setpoint",
                                             Float64,
                                             queue_size=1)
 
+    def get_setpoint(self):
+        if self.constant:
+            setpoint = self.setpoint_1
+        else:  #  square wave
+            now = rospy.get_time()
+            time = self.start_time - now
+
+            i = time % (self.duration * 2)
+            if i > (self.duration):
+                setpoint = self.setpoint_1
+            else:
+                setpoint = self.setpoint_2
+
+        self.publish_setpoint(setpoint)
+
+    def publish_setpoint(self, setpoint):
+        msg = Float64()
+        msg.data = setpoint
+        self.setpoint_pub.publish(msg)
+
     def run(self):
         rate = rospy.Rate(50.0)
         while not rospy.is_shutdown():
-            msg = Float64()
-            # fill msg with example setpoint
-            msg.data = -0.5
-            self.setpoint_pub.publish(msg)
+            self.get_setpoint()
             rate.sleep()
 
 
